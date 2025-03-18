8b8ebae406a15075e966fe692ac17009aaa3ba1d
Tri-Tacyhon <jrbenny10@gmail.com>
2025-03-17 21:29:15 -0400

    Finished Navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,15 @@
-import NavBar from './Components/NavBar';
+import NavBar from './Components/NavBar.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-    </div>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Home />
+        </div>
+      </div>
   );
 }
 
